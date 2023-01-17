580664e8ef46101b1a862e141cbc6f467a28fe06
rob3rtroessler <robertroessler@g.harvard.edu>
2023-01-16 21:49:11 -0500

    restart

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# Post Match Predictor
-
-
-to run locally:
-
-----------------
-$ export FLASK_APP=application.py
-
-$ python -m flask run
- 
--> Running on http://127.0.0.1:5000/
-
-----------------
+## RR Portfolio
---
+++ static/js/vis.js
@@ -0,0 +1,94 @@
+class Vis {
+
+    // constructor method to initialize Timeline object
+    constructor(parentElement, data) {
+        this.parentElement = parentElement;
+        this.data = data;
+        this.filteredData = []
+        this.wrangledDate = []
+        this.stopWordsSwitch = true
+
+        // call initVis method
+        this.initVis()
+    }
+
+    initVis(){
+        let vis = this;
+
+        vis.margin = {top: 10, right: 30, bottom: 50, left: 10};
+        vis.width = $("#" + vis.parentElement).width() - vis.margin.left - vis.margin.right;
+        vis.height = $("#" + vis.parentElement).height()*5 - vis.margin.top - vis.margin.bottom;
+
+        // init drawing area
+        vis.svg = d3.select("#" + vis.parentElement).append("svg")
+            .attr("width", vis.width + vis.margin.left + vis.margin.right)
+            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+            .append('g')
+            .attr('transform', `translate (${vis.margin.left}, ${vis.margin.top})`);
+
+        // add title
+        // vis.svg.append('g')
+        //     .attr('class', 'title bar-title')
+        //     .append('text')
+        //     .text('most frequent words')
+        //     .attr('transform', `translate(${vis.width / 2}, -15)`)
+        //     .attr('text-anchor', 'middle');
+
+
+        // tooltip
+        vis.tooltip = d3.select("body").append('div')
+            .attr('class', "tooltip")
+            .attr('id', 'distributionTooltip')
+
+        // axis groups
+        vis.xAxisGroup = vis.svg.append('g')
+            .attr('class', 'axis x-axis')
+            .attr('transform', `translate (0,${vis.height})`);
+
+        vis.yAxisGroup = vis.svg.append('g')
+            .attr('class', 'axis y-axis');
+
+        // having initialized the map, move on to wrangle data
+        // this.wrangleData();
+    }
+
+    wrangleData(){
+        let vis = this
+
+        vis.updateVis()
+    }
+
+    updateVis(){
+        let vis = this;
+
+
+        vis.rectanglesAway.enter().append('rect')
+            .merge(vis.rectanglesAway)
+            .attr('class', 'away-rect top-word-rect')
+            .transition()
+            .duration(500)
+            .attr('x', (d,i) => vis.width - vis.rectWidthScaleHome(d.count)) // adjust for right side
+            .attr('y', (d,i) => i*25)
+            .attr('width', d => vis.rectWidthScaleHome(d.count))
+            .attr('height', 20)
+            .style('fill', d => {
+                if (twitterSwitch) {
+                    return 'rgba(53,151,143,0.3)'
+                } else {
+                    return 'rgba(53,151,143,1)'
+                }
+            })
+            .style('stroke', d =>{
+                if (twitterSwitch) {
+                    return 'rgba(1,102,94,0.3)'
+                } else {
+                    return 'rgba(1,102,94,1)'
+                }
+            })
+
+        vis.rectanglesAway.exit().remove()
+
+    }
+
+
+}
